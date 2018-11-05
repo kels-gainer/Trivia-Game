07bacc52d12e5b47c71e7bcdcce35e9b81c5ae26
Kelsey <kelsgainer@gmail.com>
2018-11-04 17:41:48 -0700

    game

--- logic.js
+++ logic.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
 
-// clock start =====================================
 
   var countdownNumberEl = document.getElementById('countdown-number');
   var countdown = 60;
@@ -13,77 +12,85 @@ $(document).ready(function() {
 
     if(countdown === 0) {
       $("#questions").hide();
-      $("#scoresPg").show();
+      $("#scores").show();
     };
 
-    $("#sub-but").on("click", function() {
+    $(".sub-but").on("click", function() {
       clearInterval(timer);
       
   });
 
   }, 1000);
 
-//  Questions Start ========================================
 
   var questions = [{
     q: "Which planet in our solar system is the hottest?",
-    a: ["Mars", "Venus", "Mercury", "Saturn"],
+    ans: ["Mars", "Venus", "Mercury", "Saturn"],
     name: "planet",
     correct: "Venus",
-    divClass: ".planet",
   },
   {
     q: "How much does a NASA space suite cost?",
-    a: ["12 million", "1 billion", "5 thousand", "1.5 million"],
+    ans: ["12 million", "1 billion", "5 thousand", "1.5 million"],
     name: "suite",
     correct: "12 million",
-    divClass: ".suite",
   },
   {
     q: "Jupiter is the largest planet in our solar system.",
-    a: ["True", "False"],
+    ans: ["True", "False"],
     name: "size",
     correct: "True",
-    divClass: ".size",
   },
   {
     q: "How many moons does Jupiter have?",
-    a: ["32", "4", "17", "67"],
+    ans: ["32", "4", "17", "67"],
     name: "moons",
     correct: "67",
-    divClass: ".moons",
   },
   {
     q: "It takes Neptune 165 earth years to make on orbit around the sun",
-    a: ["True", "False"],
+    ans: ["True", "False"],
     name: "neptune",
     correct: "True",
-    divClass: ".neptune"
   },
   {
     q: "Which galazy is closets to our own?",
-    a: ["Milkyway", "Sombrero", "Andromeda", "Black Eye"],
+    ans: ["Milkyway", "Sombrero", "Andromeda", "Black Eye"],
     name: "galaxy",
     correct: "Andromeda",
-    divClass: ".galaxy"
   },
   ]
-
-  // Score Start ========================================================
+  
 
   $("#start-btn").on("click", function(){
     $("#startGame").hide();
     $("#questions").show();
+    $("#countdown-number").show();
+    
   });
 
-  // Grading Questions ================================================
 
   var correctAnswer = 0;
   var wrongAnswer = 0;
   var unanswered = 0;
 
-  for(var i = 0; questions.length; i++);
-  if (questions[i].checked);
-  console.log("checked");
+    for (var i = 0; i < questions.length; i++) {
+        if ($('input:radio[value="' + questions[i].ans + '"]:checked').val() === questions[i].correct) {
+            correctAnswer++;
+            console.log("are you working")
+        } else {
+            wrongAnswer++;
+            unanswered++;
+        };
+    }
+
+$(".sub-btn").on("click", function(){
+    $("#questions").hide();
+    $("#countdown-number").hide();
+    $("#scores").show();
+    $("#correctAnswers-text").append(correctAnswer);
+    $("#wrongAnswers-text").append(wrongAnswer);
+    $("#unanswered-text").append(unanswered);
+  });
   
 });
