434379bc2b7772b04f6c5153afa922019a6a3c9b
Kelsey <kelsgainer@gmail.com>
2018-11-06 20:32:24 -0700

    game update

--- logic.js
+++ logic.js
@@ -3,43 +3,29 @@ $(document).ready(function() {
 
   var countdownNumberEl = document.getElementById('countdown-number');
   var countdown = 60;
-
+  var timer;
   countdownNumberEl.textContent = countdown;
 
-  var timer = setInterval(function() {
-    countdown = --countdown <= 0 ? 60 : countdown;
-    countdownNumberEl.textContent = countdown;
-
-    if(countdown === 0) {
-      $("#questions").hide();
-      $("#scores").show();
-    };
-
-    $(".sub-but").on("click", function() {
-      clearInterval(timer);
-      
-  });
-
-  }, 1000);
+  
 
 
   var questions = [{
     q: "Which planet in our solar system is the hottest?",
-    ans: ["Mars", "Venus", "Mercury", "Saturn"],
+    ans: ["mars", "venus", "mercury", "saturn"],
     name: "planet",
-    correct: "Venus",
+    correct: "venus",
   },
   {
     q: "How much does a NASA space suite cost?",
-    ans: ["12 million", "1 billion", "5 thousand", "1.5 million"],
+    ans: ["12mil", "1bil", "5k", "1.5mil"],
     name: "suite",
-    correct: "12 million",
+    correct: "12mil",
   },
   {
     q: "Jupiter is the largest planet in our solar system.",
-    ans: ["True", "False"],
+    ans: ["true", "false"],
     name: "size",
-    correct: "True",
+    correct: "true",
   },
   {
     q: "How many moons does Jupiter have?",
@@ -49,15 +35,15 @@ $(document).ready(function() {
   },
   {
     q: "It takes Neptune 165 earth years to make on orbit around the sun",
-    ans: ["True", "False"],
+    ans: ["true", "false"],
     name: "neptune",
-    correct: "True",
+    correct: "true",
   },
   {
     q: "Which galazy is closets to our own?",
-    ans: ["Milkyway", "Sombrero", "Andromeda", "Black Eye"],
+    ans: ["milkyway", "sombrero", "andromeda", "black eye"],
     name: "galaxy",
-    correct: "Andromeda",
+    correct: "andromeda",
   },
   ]
   
@@ -66,31 +52,50 @@ $(document).ready(function() {
     $("#startGame").hide();
     $("#questions").show();
     $("#countdown-number").show();
+    timer = setInterval(function() {
+      countdown = --countdown <= 0 ? 60 : countdown;
+      countdownNumberEl.textContent = countdown;
+  
+      if(countdown === 0) {
+        $("#questions").hide();
+        $("#scores").show();
+      };
+  
+     
+        
+ 
+  
+    }, 1000);
     
   });
 
-
-  var correctAnswer = 0;
-  var wrongAnswer = 0;
-  var unanswered = 0;
-
-    for (var i = 0; i < questions.length; i++) {
-        if ($('input:radio[value="' + questions[i].ans + '"]:checked').val() === questions[i].correct) {
+function findAwnsers() {
+    console.log('submit');
+    var awnserd = $("input[type='radio']:checked");
+    console.log(awnserd);
+    for (var i = 0; i < awnserd.length; i++) {
+      
+        var index = $(awnserd[i]).attr("data-index")
+        if ($(awnserd[i]).val() === questions[index].correct) {
             correctAnswer++;
-            console.log("are you working")
         } else {
             wrongAnswer++;
-            unanswered++;
         };
     }
+  }
+ 
+  var correctAnswer = 0;
+  var wrongAnswer = 0;
+
 
 $(".sub-btn").on("click", function(){
+    clearInterval(timer);
+    findAwnsers();
     $("#questions").hide();
     $("#countdown-number").hide();
     $("#scores").show();
     $("#correctAnswers-text").append(correctAnswer);
     $("#wrongAnswers-text").append(wrongAnswer);
-    $("#unanswered-text").append(unanswered);
   });
   
 });
